6e3f951a774f4a21cfb45a8c98b4de24c000e8dc
Jackson Liu <416883813@qq.com>
2016-04-12 21:17:45 +0800

    add move fucntion

--- example/carousel/main.js
+++ example/carousel/main.js
@@ -2,21 +2,37 @@ window.onload = function() {
     var imgs = document.getElementById("imgs");
     var pre = document.getElementById("pre");
     var next = document.getElementById("next");
-    console.log(next);
-
+    var width = 800;
+    var moveTime = 600;
+    var moveCounts = 10;
 
     function move(offset) {
         var newleft = parseInt(imgs.style.left) + offset;
-        imgs.style.left = newleft + "px";
-
+        var step = offset / moveCounts;
+        var stepTime = moveTime / moveCounts;
+        var go = function() {
+            var left = parseInt(imgs.style.left);
+            var nextLeft = left + step;
+            if ((step > 0 && nextLeft < newleft) || (step < 0 && nextLeft > newleft)) {
+                imgs.style.left = nextLeft + "px";
+                setTimeout(go, stepTime);
+            } else {
+                imgs.style.left = newleft;
+                if (parseInt(imgs.style.left) == -4800) {
+                    imgs.style.left = -800;
+                } else if (parseInt(imgs.style.left) == 0) {
+                    imgs.style.left = -4000;
+                }
+            }
+        }
+        go();
     }
-
-    pre.onclick = function(){
+    pre.onclick = function() {
         move(800);
-        return false;  //return false to prevent default <a> tag link event        
+        return false; //return false to prevent default <a> tag link event
     };
-    next.onclick = function(){
+    next.onclick = function() {
         move(-800);
-        return false;  //return false to prevent default <a> tag link event 
+        return false; //return false to prevent default <a> tag link event
     };
 };
