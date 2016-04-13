03c4cb420c1770953a1a16c9bcb403db76b59267
Jackson Liu <416883813@qq.com>
2016-04-13 11:42:35 +0800

    add btn jump function

--- example/carousel/main.js
+++ example/carousel/main.js
@@ -6,6 +6,7 @@ window.onload = function() {
     var moveTime = 600;
     var moveCounts = 10;
     var moveOn = false; //move flag, prevent  move again when moving
+    var picIndex = 1;
 
     function move(offset) {
         if(moveOn) {
@@ -31,10 +32,29 @@ window.onload = function() {
                 } else if (parseInt(imgs.style.left) == 0) {
                     imgs.style.left = -4000;
                 }
+                picIndex = Math.round(parseInt(imgs.style.left)/(-800));
+                console.log(picIndex);
             }
         }
         go();
     }
+
+    function jump(ele) {
+        var btn = ele;
+        var offset = (btn.getAttribute("index") - picIndex) * (-800);
+        move(offset);
+    }
+
+    function addBtnClic(){
+        var btnsDiv = document.getElementById("btns");
+        var btns = btnsDiv.getElementsByTagName("span");
+        for(var i=0,len=btns.length; i<len; i++){
+            btns[i].onclick = function(){
+                jump(this);
+            };
+        }
+    }
+
     pre.onclick = function() {
         move(800);
         return false; //return false to prevent default <a> tag link event
@@ -43,4 +63,7 @@ window.onload = function() {
         move(-800);
         return false; //return false to prevent default <a> tag link event
     };
+
+    addBtnClic();
+
 };
