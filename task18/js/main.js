c66a3ac8dcd3008c6e22c14e53f78e6e946b91a1
Jackson Liu <416883813@qq.com>
2016-04-14 23:42:58 +0800

    some work in task18

---
+++ task18/js/main.js
@@ -0,0 +1,52 @@
+function addLoadEvent(func) {
+    var onload = window.onload;
+    if (typeof onload != 'function') {
+        onload = func;
+    } else {
+        onload = function() {
+            onload();
+            func();
+        }
+    }
+}
+
+var input = document.getElementById("input");
+var btnLeftIn = document.getElementById("left-in");
+var btnRightIn = document.getElementById("right-in");
+var btnLeftOut = document.getElementById("left-out");
+var btnRightOut = document.getElementById("right-out");
+var spanDis = document.getElementById("spans");
+var spanArry = [];
+
+function createSpan(num) {
+    var span = document.createElement("span");
+    var textnode = document.createTextNode(num);
+    span.appendChild(textnode);
+    return span;
+}
+
+function getINputNum(){
+    var num = input.value;
+    console.log(num);
+    input.value = "";
+    return isNaN(num) ? false : num;
+}
+
+function leftIn(){
+    var num = getINputNum();
+    if (isNaN(num)) return;
+    var span = createSpan(num);
+    if(0 == spanArry.length) {
+        spanDis.appendChild(span);
+    } else {
+        spanDis.insertBefore(span, spanArry[0]);
+    }
+    spanArry.unshift(span);
+}
+
+function addListener() {
+    btnLeftIn.onclick = leftIn;
+}
+addListener();
+// addLoadEvent(addListener);
+
