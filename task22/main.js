538ad4df5a674910bf2dfbdd01ecc64c132ab361
Jackson Liu <416883813@qq.com>
2016-04-20 23:20:53 +0800

    使用队列依次存储检索到的数据，然后递归显示

--- task22/main.js
+++ task22/main.js
@@ -3,12 +3,33 @@ var inputdep = document.getElementById("inputDep");
 var btncreate = document.getElementById("btnCreate");
 
 
+var nodeQuenue = (function() {
+    var q = [];
+    q.showCircle = function () {
+            if(!q.length) return false;
+            q[0].value.className = "";
+            q.shift();
+            addClassName(q[0].value, "on");
+            setTimeout(function(){q.showCircle()}, 1500);
+            };
+    return q;
+}());
+
+
+function addClassName(ele, name) {
+    if(!ele.className) {
+        ele.className = name;
+    } else {
+        oldClassNamme = ele.className;
+        ele.className = oldClassNamme + " " + name;
+    }
+}
+
 function createEle (str) {
      var ele = document.createElement(str);
      if(!ele ) return null;
      return ele;
 }
-
 
 //二叉树构造函数
 function Node (ele) {
@@ -30,18 +51,26 @@ function createNodeTree(root, depth) {
 //二叉树转成具有父子关系的DOM元素
 function tree2Dom (root) {
     if(null == root) return false;
-    var rootspan = root.value;
+    var rootdiv = root.value;
     if(root.leftnode != null) {
-        var leftspan = root.leftnode.value;
-        rootspan.appendChild(leftspan);
+        var leftdiv = root.leftnode.value;
+        rootdiv.appendChild(leftdiv);
         tree2Dom(root.leftnode);
     }
     if(root.rightnode != null) {
-        var rightspan = root.rightnode.value;
-        rootspan.appendChild(rightspan);
+        var rightdiv = root.rightnode.value;
+        rootdiv.appendChild(rightdiv);
         tree2Dom(root.rightnode);
     }
 
+ }
+
+//前序遍历
+ function preOrder(root) {
+    if(null == root) return false;
+    nodeQuenue.push(root);
+    preOrder(root.leftnode);
+    preOrder(root.rightnode);
  }
 
 //添加监听事件
@@ -58,8 +87,11 @@ function tree2Dom (root) {
         createNodeTree(root, parseInt(text));  //根据深度创建二插树
         container.innerHTML = "";
         container.style.display = "block";
+        container.className = "on";
         //将二叉树结构转成DOM元素，即将子元素append到父元素中
         tree2Dom(root);
         console.log(root);
+        preOrder(root);
+        nodeQuenue.showCircle();
     }
  }());
