8ceed684b0cbe9e57c22cbf654f0e3b06fc15c71
Jackson Liu <416883813@qq.com>
2016-04-21 12:30:17 +0800

    添加遍历演示按钮

--- task22/main.js
+++ task22/main.js
@@ -1,20 +1,38 @@
 var container = document.getElementById("container");
 var inputdep = document.getElementById("inputDep");
 var btncreate = document.getElementById("btnCreate");
+var btntraverse = document.getElementById("btntraverse");
 
+
+//二叉树节点构造函数
+function Node (ele) {
+    this.value = ele;
+    this.leftnode = null;
+    this.rightnode = null;
+}
+
+//二叉树构造函数
+function Tree(root) {
+    var node = new Node(root);
+    this.root = node; 
+}
 
 var nodeQuenue = (function() {
     var q = [];
     q.showCircle = function () {
             if(!q.length) return false;
-            q[0].value.className = "";
-            q.shift();
+            //依次显示第一个元素，然后剔除
+            //如果第一个元素已经显示了，就设为不显示，剔除队列
+            if(q[0].value.className.indexOf("on") != -1) {
+                q[0].value.className = "";
+                q.shift();
+            }
+            //显示第一个元素
             addClassName(q[0].value, "on");
             setTimeout(function(){q.showCircle()}, 1500);
             };
     return q;
 }());
-
 
 function addClassName(ele, name) {
     if(!ele.className) {
@@ -25,18 +43,12 @@ function addClassName(ele, name) {
     }
 }
 
-function createEle (str) {
-     var ele = document.createElement(str);
+function createEle (label) {
+     var ele = document.createElement(label);
      if(!ele ) return null;
      return ele;
 }
 
-//二叉树构造函数
-function Node (ele) {
-    this.value = ele;
-    this.leftnode = null;
-    this.rightnode = null;
-}
 
 //根据深度创建二叉树
 function createNodeTree(root, depth) {
@@ -76,6 +88,7 @@ function tree2Dom (root) {
 //添加监听事件
  (function addlistener() {
     //生成二叉树的按键事件
+    var root = new Node(container); //创建根元素
     btncreate.onclick = function () {
         var text = inputdep.value;
         //检查深度输入的有效性
@@ -83,14 +96,16 @@ function tree2Dom (root) {
             alert("输入数字");
             return;
         }
-        var root = new Node(container); //创建根元素
         createNodeTree(root, parseInt(text));  //根据深度创建二插树
         container.innerHTML = "";
         container.style.display = "block";
-        container.className = "on";
         //将二叉树结构转成DOM元素，即将子元素append到父元素中
         tree2Dom(root);
         console.log(root);
+        
+    };
+
+    btntraverse.onclick = function () {
         preOrder(root);
         nodeQuenue.showCircle();
     }
