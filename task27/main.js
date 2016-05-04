51ad255d91b526334783cb66876b90ade337bf02
Jackson Liu <416883813@qq.com>
2016-05-04 21:40:06 +0800

    init task27

---
+++ task27/main.js
@@ -0,0 +1,373 @@
+function $ (id) {
+    return document.getElementById(id);
+}
+
+function log (str) {
+    console.log(str);
+}
+
+function Canvas(canvasEle) {
+    this.canvas = canvasEle;
+    this.width = canvasEle.width;
+    this.height = canvasEle.height;
+    this.c = canvasEle.getContext("2d");
+}
+
+//以xy为原点，调用callback进行绘制，然后顺时针旋转角度angle
+Canvas.prototype.rotate = function (x, y, angle, callback) {
+    this.c.save();
+    this.c.translate(x, y);
+    this.c.rotate(angle);
+    callback();
+    this.c.translate(0, 0);
+    this.c.restore();
+}
+Canvas.prototype.setBackground = function (rgba) {
+    this.c.fillStyle = rgba;
+    this.c.fillRect(0, 0, this.width, this.height);
+}
+
+Canvas.prototype.drawLine = function (x1, y1, x2, y2, width, rgba, linecap) {
+    this.c.lineCap = linecap;  //butt,round, square
+    this.c.lineWidth = width;
+    this.c.strokeStyle = rgba;
+    this.c.beginPath();
+    this.c.moveTo(x1, y1);
+    this.c.lineTo(x2, y2);
+    this.c.closePath();
+    this.c.stroke();
+}
+
+Canvas.prototype.drawCircle = function (x, y, radius, rgba) {
+    this.c.beginPath();
+    this.c.fillStyle = rgba;
+    this.c.arc(x, y, radius, 0, 2 * Math.PI, false);
+    this.c.closePath();
+    this.c.fill();
+}
+
+Canvas.prototype.drawText = function (str, rgba, x, y) {
+    this.c.font = "bold 14px Arial";
+    this.c.textAlign = "center";
+    this.c.textBaseline = "middle";
+    this.c.fillStyle = rgba;
+    this.c.fillText(str, x, y);
+}
+
+function Position (x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function Entity(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+}
+
+Entity.prototype.addTo = function (world) {
+    world.add(this);
+}
+
+Entity.prototype.destory = function () {
+    if (!this.world) return;
+    this.world.destory(this);
+}
+
+
+function Star(x, y, radius, color) {
+    Entity.call(this, x, y);
+    this.radius =radius;
+    this.color = color;
+}
+Star.prototype = Object.create(Entity.prototype);
+Star.prototype.constructor = Star;
+Star.prototype.show = function () {
+    if (!this.world) return false;
+    this.canvas.drawCircle(this.x, this.y, this.radius, this.color);
+}
+
+function Ship(x, y, angle, energy, speed, chargerate) {
+    Entity.call(this, x, y);
+    this.angle = angle || 0;  //围绕star的角度
+    this.energy = energy || 100; //默认100%
+    this.shape = {
+        width: 30,
+        length: 70
+    };
+    this.speed = speed || 5; //速度是每秒多少度(一圈360度)
+    this.isstop = true;
+    this.powerrate = 1; //每1度耗费百分之多少
+    this.chargerate = chargerate || 3; //每秒充电百分之多
+}
+Ship.args = {
+    speed: {
+        march: 5,
+        rush: 7,
+        beyond: 9
+    },
+    chargerate: {
+        normal: 3,
+        light: 4,
+        everlasting: 5
+    }
+}
+
+Ship.prototype = Object.create(Entity.prototype);
+Ship.prototype.constructor = Ship;
+//旋转需要更改自身的角度
+Ship.prototype.rotate = function (angle) {
+    this.angle = this.angle + angle;
+};
+//每一隔一段时间调用该step函数进行移动
+//内部规定如何移动
+Ship.prototype.step = function () {
+    if (this.energy < 100) {
+        this.energy += this.chargerate * this.world.steptiming;
+        if (this.energy > 100) {
+            this.energy = 100;
+        }
+    }
+    if (this.isstop) return;    //如果停止，则不运动
+    var moveAngle = this.speed * this.world.steptiming / 180 * Math.PI;
+    this.angle += moveAngle;
+    var consume = this.powerrate * this.speed * this.world.steptiming;
+    this.energy -= consume;
+    if ( this.energy <= 0) {
+        this.energy = 0;
+        this.enable(false);
+    }
+};
+Ship.prototype.show = function () {
+    var halflen = this.shape.length/2;
+    var width = this.shape.width;
+    var headx = -halflen;
+    var heady = -this.star.radius - 50;
+    var endx = halflen;
+    var endy = -this.star.radius - 50;
+    var that = this;
+    var statstr = this.id + "号-" + parseInt(this.energy) + "%";
+    var drawShip = function() {
+        that.canvas.drawCircle(endx,endy,width/2, "blue");
+        that.canvas.drawLine(headx, heady, endx, endy, width, "#555", "round");
+        that.canvas.drawText(statstr, "#000", 0, endy);
+    }
+    this.canvas.rotate(this.star.x, this.star.y, this.angle, drawShip);
+
+}
+Ship.prototype.attach2Star = function (star) {
+    this.star = star;
+}
+Ship.prototype.enable = function (start) {
+    start ? this.isstop = false : this.isstop = true;
+}
+Ship.prototype.handleCommond = function (commmond) {
+    console.log("ship [" + this.id+"] get commmond = " + commmond);
+    if (this.id != commmond.id) return;
+    var order = commmond.order;
+    switch (order) {
+        case "stop":
+            this.enable(false);
+            break;
+        case "destory":
+            this.destory();
+            break;
+        case "start":
+            this.enable(true);
+        default:
+            // statements_def
+            break;
+    }
+}
+
+function Commonder (x, y) {
+    Entity.call(this, x, y);
+    this.ships = [];
+}
+
+Commonder.prototype = Object.create(Entity.prototype);
+Commonder.prototype.constructor = Commonder;
+Commonder.prototype.createShip = function (star, angle) {
+    if (this.ships.length >= 4) return false;
+    var ship = new Ship(this.world.x, this.world.y , angle);
+    var indexs = [1,2,3,4];
+    var that = this;
+    console.log(this.ships.length);
+    for(var i=0,len=this.ships.length;i<len;i++){
+        indexs.forEach(function(ele,index) {
+            if (that.ships[i].id == ele) {
+                console.log("that.ships[i].id " + that.ships[i].id);
+                console.log(indexs);
+                indexs.splice(index, 1);
+            }
+        })
+    }
+    ship.id = indexs[0];
+    ship.addTo(this.world);
+    ship.attach2Star(star);
+    this.ships.push(ship);
+    return ship;
+}
+Commonder.prototype.destoryShip = function (index) {
+    var that = this;
+    this.ships.forEach(function(ele,i) {
+        if (ele.id == index) {
+            that.ships.splice(i, 1);
+        }
+    })
+}
+Commonder.prototype.sendCommond = function (index, order) {
+    if (!this.world.Ship) return;
+    if (index > 4 || !order) return;
+    var that = this;
+    var commmond = {
+        id: index,
+        order: order
+    };
+    var spreadCmd = function () {
+        that.world.Ship.forEach( function(element, index) {
+        if (!element.handleCommond) return;
+        //模拟丢包率，有30%几率接收不到信息
+        if (Math.random() < 0.3) {
+            return;
+        }
+        element.handleCommond(commmond);
+        });
+    };
+    setTimeout(spreadCmd, 1000); //传播信号需要1s
+}
+
+function World (canvas) {
+    this.canvas = new Canvas(canvas);
+    this.c = this.canvas.c;
+    this.entites = [];
+    this.steptiming = 0.02; //world内部实体运动的最小间隔时间/秒
+    this.showtiming = 0.02;  //world显示美帧的间隔,/秒
+}
+World.prototype.add = function (entity) {
+    entity.world = this;
+    entity.canvas = this.canvas;
+    this.entites.push(entity);  //加入world.entities数组
+    var entityType = entity.constructor.name;
+    if (!this[entityType]) {
+        this[entityType] = [];
+    }
+    this[entityType].push(entity); //push到特定类型的数组
+}
+World.prototype.destory = function (entity) {
+    var index = this.entites.indexOf(entity);
+    if (index == -1) return;
+    this.entites.splice(index, 1);
+    var entityTypeArry = this[entity.constructor.name]
+    index = entityTypeArry.indexOf(entity);
+    entityTypeArry.splice(index, 1);
+}
+World.prototype.runStep = function () {
+    //对每个有step的物体进行移动
+    var ents = this.entites;
+        for(var i=0, len=ents.length;i<len;i++) {
+            var ent = ents[i];
+            if(ent.step) {
+                ent.step();
+            }
+        }
+} 
+
+World.prototype.show = function () {
+    this.canvas.setBackground("black");//先显示背景
+     var ents = this.entites;
+     //依次显示world内的实体
+        for(var i=0, len=ents.length;i<len;i++) {
+            var ent = ents[i];
+            if(ent.show) {
+                ent.show();
+            }
+        }
+}
+
+World.prototype.enableRun = function (enable) {
+    if (enable) {
+        var steptimer = setInterval(this.runStep.bind(this), this.steptiming*1000);
+        var showtimer = setInterval(this.show.bind(this), this.showtiming*1000); 
+    } else {
+        clearInterval(steptimer);
+        clearInterval(showtimer);
+    }
+}
+
+function Shipctldiv (id) {
+    var div = document.createElement("div");
+    div.className = "ship";
+    div.setAttribute("index", id);
+    var label = document.createElement("label");
+    label.innerHTML = id + "号飞船";
+    var btnstart = document.createElement("button");
+    btnstart.name = "start";
+    btnstart.innerHTML = "启动";
+    var btnstop = document.createElement("button");
+    btnstop.name = "stop";
+    btnstop.innerHTML = "停止";
+    var btndestory = document.createElement("button");
+    btndestory.innerHTML = "摧毁";
+    btndestory.name = "destory";
+    div.appendChild(label);
+    div.appendChild(btnstart);
+    div.appendChild(btnstop);
+    div.appendChild(btndestory);
+    return div;
+}
+
+function main() {
+    var container = $("container");
+    var monitor = $("monitor");
+    var control = $("control");
+    var btncreate = $("btncreate");
+    var divships = $("ships");
+    var world = new World(monitor);
+    var star = new Star(200,200,100,"blue");
+    var commonder = new Commonder(0, 0);
+    commonder.addTo(world);
+    star.addTo(world);
+    world.enableRun(true);
+
+    btncreate.onclick = function (event) {
+        var ship = commonder.createShip(star, 0);
+        if (!ship) return;
+        var shipctldiv = Shipctldiv(ship.id);
+        ships.appendChild(shipctldiv);
+    }
+
+    //事件委托，在ships div中处理ship的按钮事件
+    divships.onclick = function (event) {
+        var shipdiv = event.target.parentNode;
+        var index = shipdiv.getAttribute("index");
+        var order = event.target.name;
+        commonder.sendCommond(index, order);
+        switch (order) {
+            case "destory": shipdiv.parentNode.removeChild(shipdiv);
+                commonder.destoryShip(index);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
+
+//console.log(navigator.userAgent);  
+//如果在jest中会显示”Node.js (darwin; U; rv:v5.3.0)“
+function isTestMode() {
+    return (navigator.userAgent.indexOf("Node") != -1) ;
+}
+
+if (isTestMode()) {
+    module.exports = {
+        Canvas: Canvas,
+        Entity: Entity,
+        Star: Star,
+        Ship: Ship,
+        World: World,
+        Commonder: Commonder
+    }    
+} else {
+    main();
+}
